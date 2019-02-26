afc9b1726234b90b008cee23ca90c4e7e396877e
apri <apriyantotobing8@gmail.com>
2019-02-26 17:48:32 +0700

    Add files via upload
    Versi Perdana

---
+++ assets/js/mycustom.js
@@ -0,0 +1,18 @@
+(() => {
+  'use strict';
+  // Page is loaded
+  const objects = document.getElementsByClassName('asyncImage');
+
+  Array.from(objects).map((item) => {
+    // Start loading image
+    const img = new Image();
+    img.src = item.dataset.src;
+    // Once image is loaded replace the src of the HTML element
+    img.onload = () => {
+      item.classList.remove('asyncImage');
+      return item.nodeName === 'IMG' ? 
+        item.src = item.dataset.src :        
+        item.style.backgroundImage = `url(${item.dataset.src})`;
+    };
+  });
+})();
